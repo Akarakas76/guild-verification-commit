f6e0aaf3b1f5c46ae693eb0701b429420af7205b
Abdullah Karakaş <103152374+Akarakas76@users.noreply.github.com>
2025-10-30 23:13:37 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T20:01:34.533Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-10-30T20:13:31.940Z
+Commit: Patch API endpoint error handling
